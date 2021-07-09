174dfc70a1f000ca1195a271cc2b58abb9c7e871
Dean Rieck <ddrieck@gmail.com>
2021-07-09 12:56:01 -0700

    Updated version number.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="golden_hour",
-    version="1.1.0",
+    version="1.2.0",
     description="Record a sunset timelapse and post it to Twitter with a weather report",
     long_description=long_description,
     long_description_content_type="text/markdown",
